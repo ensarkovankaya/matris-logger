2a862df94f7d32e9a5bcfe1c0afe94fb43b6e2da
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-31 12:54:56 +0300

    add pino-pretty

--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-export const isTest = (): boolean => String(process.env.NODE_ENV).toLowerCase() === 'test';
-
-export const isDevelopment = (): boolean => {
-    const env = String(process.env.NODE_ENV).toLowerCase();
-    return env === 'dev' || env === 'development';
-};
