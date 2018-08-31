a7f4b26c2550a9bdcab2f6c35df3529fe419d12f
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-31 12:55:14 +0300

    change user serializer

--- lib/serializers/user.serializer.ts
+++ lib/serializers/user.serializer.ts
@@ -3,8 +3,9 @@
  * Removes sensitive user information from user object.
  * @param value
  */
-export const userSerializer = (value: any) => {
-    if ('password' in value) {
+export const userSerializer = (value: any): any => {
+    if (typeof value === 'object' && 'password' in value) {
         return {...value, password: undefined};
     }
+    return value;
 };
